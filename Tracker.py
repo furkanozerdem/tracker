e36118ebf9231047a928646954b34a7dba0bdea9
İbrahim Halil Özerdem <i.h.ozerdem@gmail.com>
2025-12-31 22:25:31 +0300

    [upd] Geliştirme güncellendi.

--- Tracker.py
+++ Tracker.py
@@ -8,17 +8,34 @@ tracker = cv2.TrackerCSRT_create()
 time.sleep(2)
 success, img = cap.read() 
 
+if not success:
+    print("Kamera görüntüsü alınamadı.")
+    exit()
+
 bounding_box = cv2.selectROI("Tracking",img,False)
-tracker.init(img,bounding_box)
+print("Seçilen bounding box:", bounding_box)
+
+if bounding_box[2] > 0 and bounding_box[3] > 0:
+    # Görüntü boyutlarını kontrol et
+    height, width = img.shape[:2]
+    print("Görüntü boyutu:", (width, height))
+    
+    # Bounding box'u int'e çevir ve sınırları kontrol et
+    bounding_box = tuple(int(v) for v in bounding_box)
+    if bounding_box[0] + bounding_box[2] > width or bounding_box[1] + bounding_box[3] > height:
+        print("Bounding box görüntü sınırları dışında.")
+        exit()
+    
+    tracker.init(img, bounding_box)
+else:
+    print("Geçersiz bounding box seçildi.")
+    exit()
 
 def drawBox(img, bbox):  #bbox içinde 4 int içeren bir tupledır
    
     x, y, w, h = int(bbox[0]),int(bbox[1]), int(bbox[2]), int(bbox[3])
     cv2.rectangle(img,(x,y),((x+w),(y+h)),(0,255,0),3,1 )
     cv2.putText(img,"Tracking",(400,50),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
-
-
-
 
 while True:
     timer = cv2.getTickCount()
@@ -30,7 +47,6 @@ while True:
         drawBox(img,bounding_box)
     else:
         cv2.putText(img,"There is no object",(400,50),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,8,231),2)
-
 
     fps = cv2.getTickFrequency() / (cv2.getTickCount()-timer)
     cv2.putText(img,str(int(fps)),(75,50),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,8,231),2)
